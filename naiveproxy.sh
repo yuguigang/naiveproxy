6127ea94ab3eeb5bd3167c249a499d4fac241b0c
taffychan <taffychan@protonmail.com>
2022-08-09 11:52:48 +0800

    Update

--- naiveproxy.sh
+++ naiveproxy.sh
@@ -312,6 +312,7 @@ menu() {
     read -rp " 请输入选项 [0-13] ：" answer
     case $answer in
         1) installProxy ;;
+        2) uninstallProxy ;;
         *) red "请输入正确的选项 [0-13]！" && exit 1 ;;
     esac
 }
