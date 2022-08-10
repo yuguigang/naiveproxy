5be7970bfef0e8a55265106a24e63a99d8e74e13
taffychan <taffychan@protonmail.com>
2022-08-10 22:52:18 +0800

    Update

--- naiveproxy.sh
+++ naiveproxy.sh
@@ -68,7 +68,7 @@ buildCaddy(){
     go install github.com/caddyserver/xcaddy/cmd/xcaddy@latest
     ~/go/bin/xcaddy build --with github.com/caddyserver/forwardproxy@caddy2=github.com/klzgrad/forwardproxy@naive
     rm -rf go
-    mv caddy /usr/bin/caddy
+    mv ./caddy /usr/bin/caddy
 }
 
 makesite(){
